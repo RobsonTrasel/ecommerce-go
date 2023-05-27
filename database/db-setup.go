cb12d46283d2df88c08a7a46f66a49709b37b772
RobsonTrasel <robson.trasel@gmail.com>
2023-05-26 23:26:08 -0300

    chore(database): commit again, bcz i want :)

--- database/db-setup.go
+++ database/db-setup.go
@@ -1,18 +1,55 @@
 package database
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func DBConfig() *mongo.Client {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Println("Failed to connect to MongoDB")
+		return nil
+	}
+
+	fmt.Println("Successfully connected to MongoDB")
+
+	return client
+
 }
 
-func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+var Client *mongo.Client = DBConfig()
 
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+
+	return collection
 }
 
 func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var productCollection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
 
+	return productCollection
 }
