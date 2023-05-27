8667247aca171fee55dc01528be9478f24c65cf3
RobsonTrasel <robson.trasel@gmail.com>
2023-05-27 01:18:08 -0300

    chore(controller): add DeleteAddress controller to address controller

---
+++ controllers/address.go
@@ -0,0 +1,53 @@
+package controllers
+
+import (
+	"context"
+	"ecommerce-go/models"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func AddAddress() gin.HandlerFunc {
+
+}
+
+func EditHomeAddress() gin.HandlerFunc {
+
+}
+
+func EditWorkAddress() gin.HandlerFunc {
+
+}
+
+func DeleteAddress() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user_id := c.Query("id")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"Error": "Invalid Search Index"})
+			c.Abort()
+			return
+		}
+		addresses := make([]models.Address, 0)
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(500, "Internal Server Error")
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		filter := bson.D{primitive.E{Key: "_id", Value: usert_id}}
+		update := bson.D{{Key: "$set", Value: bson.D{primitive.E{Key: "address", Value: addresses}}}}
+		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.IndentedJSON(404, "Wrong")
+			return
+		}
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(200, "Successfully deleted")
+	}
+}
