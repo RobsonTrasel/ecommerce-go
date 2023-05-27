8bc1b376b90a65f9865792aec0a86fa02afa2bde
RobsonTrasel <robson.trasel@gmail.com>
2023-05-27 00:58:44 -0300

    chore(controller): add last method ProductViewAdmin

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -157,7 +157,23 @@ func Login() gin.HandlerFunc {
 }
 
 func ProductViewAdmin() gin.HandlerFunc {
-
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var products models.Product
+		defer cancel()
+		if err := c.BindJSON(&products); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		products.ProductID = primitive.NewObjectID()
+		_, anyerr := ProductCollection.InsertOne(ctx, products)
+		if anyerr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Not Created"})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, "Successfully added our product Admin")
+	}
 }
 
 func SearchProduct() gin.HandlerFunc {
