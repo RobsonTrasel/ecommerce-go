9c38c61668959c650339ee1223449b3dfd378a1f
RobsonTrasel <robson.trasel@gmail.com>
2023-05-26 22:08:45 -0300

    chore(methods): implement some methods on main controller

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,3 +1,31 @@
 package controllers
 
-func
+import "github.com/gin-gonic/gin"
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword string, paramPassword string) (bool, string) {
+
+}
+
+func SignUp() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+
+}
+
+func ProductViewAdmin() gin.HandlerFunc {
+
+}
+
+func SearchProduct() gin.HandlerFunc {
+
+}
+
+func SearchProductByView() gin.HandlerFunc {
+
+}
